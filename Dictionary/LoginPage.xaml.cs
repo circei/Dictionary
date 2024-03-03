0ec52a14eaaf18e2c87fd107bfa596bb0d773e4c
Circei Andrei <andreicircei78@gmail.com>
2024-03-03 11:24:19 +0200

    added main window buttons

--- Dictionary/LoginPage.xaml.cs
+++ Dictionary/LoginPage.xaml.cs
@@ -16,18 +16,13 @@ using System.Windows.Shapes;
 namespace Dictionary
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginPage : Page
     {
-        public MainWindow()
+        public LoginPage()
         {
             InitializeComponent();
-        }
-        private void AdminButton_Click(object sender, RoutedEventArgs e)
-        {
-            LoginPage loginPage = new LoginPage();
-            this.Content = loginPage;
         }
     }
 }
