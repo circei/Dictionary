e3519a280b24968d19d6a42d6af3449146305b5f
Circei Andrei <andreicircei78@gmail.com>
2024-03-03 23:10:51 +0200

    added login page and login page logic

--- Dictionary/LoginPage.xaml.cs
+++ Dictionary/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Dictionary
 {
@@ -20,9 +23,65 @@ namespace Dictionary
     /// </summary>
     public partial class LoginPage : Page
     {
+        private List<User> users;
         public LoginPage()
         {
             InitializeComponent();
+
+            LoadUsers();
+        }
+        private void LoadUsers()
+        {
+            try
+            {
+                string jsonText = File.ReadAllText("..\\credentials.json");
+
+                JObject json = JObject.Parse(jsonText);
+
+                users = new List<User>();
+                JArray usersArray = (JArray)json["users"];
+                foreach (var userJson in usersArray)
+                {
+                    string username = userJson["username"].ToString();
+                    string password = userJson["password"].ToString();
+                    users.Add(new User(username, password));
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Credentials file not found.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading credentials: {ex.Message}");
+            }
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            bool login = false;
+
+            foreach(var user in users)
+            {
+                if(user.Username == username && user.Password == password)
+                {
+                    login = true;
+                    break;
+                }
+            }
+            if (login)
+            {
+                lblMessage.Text = "Login successful!";
+            }
+            else
+            {
+                lblMessage.Text = "Invalid username or password. Please try again.";
+            }
+            /*LoginPage loginPage = new LoginPage();
+            this.Content = loginPage;
+            */
         }
     }
 }
